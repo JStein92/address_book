d76c846c8a17499b20673b2a35336d689358d7f2
js <jonathanstein@live.com>
2017-08-04 09:52:09 -0700

    first commit for initial files and structure

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using JobHistory.Models;
+using System.Collections.Generic;
+using System;
+using System.Globalization;
+
+namespace AddressBook.Controllers
+{
+  public class HomeController : Controller
+  {
+
+  }
+}
